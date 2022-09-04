3c58a8ca20fc826047076cfb1aefb37b1ae131cf
Jack Atkerson <jatkerson18@gmail.com>
2022-09-04 15:59:07 -0500

    Basics of Nav set up

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,52 @@
 import React from 'react';
-import Nav from './components/Nav';
 
-function App() {
+function Nav() {
+    const categories = [
+        {
+            name: 'Portfolio',
+            description: 'A selection of the websites and applications I have developed'
+        },
+        {
+            name: 'Resume',
+            description: 'My resume, listing my education, expreience, and qualifications'
+        }
+    ]
+
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+    }
+
     return (
-        <div>
-            <Nav></Nav>
-            <main>
-
-            </main>
-        </div>
+        <header>
+            <h2>
+                <a href="/">
+                    Jack Atkerson
+                </a>
+            </h2>
+            <nav>
+                <ul className='flex-row'>
+                    <li className='mx-2'>
+                        <a href='#about'>
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        <li
+                            className='mx-1'
+                            key={category.name}
+                        >
+                            <span onClick={() => categorySelected(category.name)}>
+                                {category.name}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
     );
 }
 
-export default App;
+export default Nav;
