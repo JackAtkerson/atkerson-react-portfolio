8ca5b2a520db4801b0e4f16b961b6fcc90177437
Jack Atkerson <jatkerson18@gmail.com>
2022-09-04 15:00:48 -0500

    Started About component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import About from './components/About';
 
 function App() {
   return (
     <div>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+            <h1 id='about'>This is me</h1>
+        </section>
+    );
+}
+
+export default About;
