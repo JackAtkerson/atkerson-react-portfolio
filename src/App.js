8fdf4777199237fb582b2e44b58f800f0476d878
Jack Atkerson <jatkerson18@gmail.com>
2022-09-04 18:11:13 -0500

    Controlled contact state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Project from './components/Project';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Project></Project>
         <About></About>
       </main>
