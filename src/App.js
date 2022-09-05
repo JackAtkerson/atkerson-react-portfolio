22274f39ef756859603806a9b72065297db7a1fc
Jack Atkerson <jatkerson18@gmail.com>
2022-09-05 15:41:47 -0500

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import './App.css';
 
 function App() {
+  const [currentCategory, setCurrentCategory] = useState('about');
+
+  const renderPage = () => {
+    switch (currentCategory) {
+      case 'about':
+        return <About />;
+      case 'portfolio':
+        return <Portfolio />;
+      case 'resume':
+        return <Resume />;
+      case 'contact':
+        return <Contact />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div>
-      <main>
-        <Header></Header>
-      </main>
+      <div>
+        <Header
+          currentCategory = {currentCategory}
+          setCurrentCategory = {setCurrentCategory}
+        ></Header>
+      </div>
+      <div>
+        <main>{renderPage()}</main>
+      </div>
+      <div>
+      </div>
     </div>
   );
 }
