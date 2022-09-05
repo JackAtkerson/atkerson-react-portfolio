c7644024198d709a325699cac57a75bf8cae25e2
Jack Atkerson <jatkerson18@gmail.com>
2022-09-05 15:26:48 -0500

    Reworked header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Nav from '../Nav';
-import About from '../About';
-import Portfolio from '../Portfolio';
-import Contact from '../Contact';
-import Resume from '../Resume';
 
-function Header() {
-    const [currentPage, setCurrentPage] = useState('About');
-
-    const renderPage = () => {
-        switch (currentPage) {
-            case 'About':
-                return <About />;
-            case 'Portfolio':
-                return <Portfolio />;
-            case 'Contact':
-                return <Contact />;
-            case 'Resume':
-                return <Resume/>;
-
-            default:
-                return <About />;
-        }
-    };
+function Header(props) {
+    const {currentPage, setCurrentPage} = props;
 
     return (
-        <div>
-            <Nav
-                currentPage = {currentPage}
-                setCurrentPage = {setCurrentPage}
-            />
-            <main>
-                <div>
-                    {renderPage(currentPage)}
-                </div>
-            </main>
-        </div>
-    )
+        <header>
+            <div>
+                <Nav
+                    currentPage = {currentPage}
+                    setCurrentPage = {setCurrentPage}
+                ></Nav>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
