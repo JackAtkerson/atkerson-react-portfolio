1a03b7ba58652864ae547b63b4e4153d75d90c28
Jack Atkerson <jatkerson18@gmail.com>
2022-09-05 15:05:51 -0500

    Worked on portfolio component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <section className='my-5'>
             <h1 id='about'>This is me</h1>
-            <img src={aboutImage} className='my-2' style={{ width:'100%' }} alt='Headshot of Jack Atkerson holding balloons' />
+            <img src={aboutImage} className='my-2' style={{ width:'100%' }} alt='Jack Atkerson' />
         </section>
     );
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,70 +1,11 @@
 import React from 'react';
-import Project from '../Project';
-
-const projects = [
-    {
-        name: 'Horiseon',
-        image: '',
-        description: '',
-        repo: '',
-        link: ''
-    },
-    {
-        name: 'Pitcher Perfect',
-        image: '',
-        description: '',
-        repo: '',
-        link: ''
-    },
-    {
-        name: 'Ship Space',
-        image: '',
-        description: '',
-        repo: '',
-        link: ''
-    },
-    {
-        name: 'Run Buddy',
-        image: '',
-        description: '',
-        repo: '',
-        link: ''
-    },
-    {
-        name: 'Robot Gladiators',
-        image: '',
-        description: '',
-        repo: '',
-        link: ''
-    },
-    {
-        name: 'Budget Tracker',
-        image: '',
-        description: '',
-        repo: '',
-        link: ''
-    },
-    {
-        name: 'Password Generator',
-        image: '',
-        description: '',
-        repo: '',
-        link: ''
-    },
-    {
-        name: 'README Generator',
-        image: '',
-        description: '',
-        repo: '',
-        link: ''
-    }
-];
+import Projects from '../Projects';
 
 function Portfolio() {
     return (
         <div>
             <h3>Portfolio</h3>
-            <Project projects={projects} />
+            <Projects />
         </div>
     );
 }
