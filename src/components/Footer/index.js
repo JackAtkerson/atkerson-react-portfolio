1304276e1c3c64f6ffbfcd2ed66f5fec103cdadf
Jack Atkerson <jatkerson18@gmail.com>
2022-09-06 16:01:03 -0500

    fixed header

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <p>I Hate this Project so fucking much</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
