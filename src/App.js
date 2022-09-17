0f2d743fa7390837cb77d5bf611d73ac40d029cf
Jack Atkerson <jatkerson18@gmail.com>
2022-09-17 11:30:42 -0400

    Reworking App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import Header from './components/Header'
+import React, { useState } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
+const [currentPage, setCurrentPage] = useState('about');
+
+const renderPage = () => {
+  switch (currentPage) {
+    case 'about':
+      return <About />;
+    case 'portfolio':
+      return <Portfolio />;
+    case 'resume':
+      return <Resume />;
+    case 'Contact':
+      return <Contact />;
+    default: return null;
+  }
+};
+
   return (
     <div>
-      <Header></Header>
-      <Footer></Footer>
+      <div>
+        <Header currentPage={currentPage} setCurrentPage={setCurrentPage}></Header>
+      </div>
+      <div>
+        <main>{renderPage()}</main>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,12 @@
 import React, { useState } from 'react';
 import Nav from '../Nav';
-import About from '../About';
-import Contact from '../Contact';
-import Portfolio from '../Portfolio'
-import Resume from '../Resume';
 
-function Header() {
-    const [currentPage, handlePageChange] = useState('About');
-
-    const renderPage = () => {
-        switch (currentPage) {
-            case 'About': return <About></About>;
-            case 'Portfolio': return <Portfolio></Portfolio>;
-            case 'Resume': return <Resume></Resume>;
-            case 'Contact': return <Contact></Contact>;
-            default: return <About></About>;
-        }
-    }
-
+function Header(props) {
+    const { currentPage, setCurrentPage } = props;
     return (
-        <div>
-            <nav>
-                <div>
-                    <a
-                        className='navbar-item'
-                        rel='noreferrer'
-                        target='_blank'
-                        href='https://github.com/JackAtkerson?tab=repositories'
-                    >
-                        <span>Jack Atkerson</span>
-                    </a>
-                </div>
-            </nav>
-            <Nav
-                currentPage = {currentPage}
-                handlePageChange = {handlePageChange}
-            />
-            <main>
-                <div>{renderPage(currentPage)}</div>
-            </main>
-        </div>
+        <header>
+            
+        </header>
     );
 }
 
