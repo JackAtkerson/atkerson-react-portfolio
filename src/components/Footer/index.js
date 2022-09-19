10d5dd68e20ff601692bc00aaa6f52d1b0561277
Jack Atkerson <jatkerson18@gmail.com>
2022-09-19 17:11:49 -0500

    Added footer logos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,11 +5,21 @@ function Footer() {
         <footer className='footer'>
             <div className='content has-text-centered'>
                 <p>
-                    <span>🔥Websites by Jack, for Jack🔥</span>
-            <br />
-          <a href="https://www.linkedin.com/in/jack-atkerson-79a505132/" target="_blank" rel="noreferrer">LinkedIn</a>
-          |
-          <a href="https://github.com/JackAtkerson" target="_blank" rel="noreferrer">GitHub</a>
+                    <a href="https://www.linkedin.com/in/jack-atkerson-79a505132/" target="_blank" rel="noreferrer">
+                        <img className='ftr-link' src={require('../../assets/images/linkedin.png')} alt='LinkedIn'></img>
+                    </a>
+
+                    <a href="https://github.com/JackAtkerson" target="_blank" rel="noreferrer">
+                        <img className='ftr-link' src={require('../../assets/images/github.png')} alt='GitHub'></img>
+                    </a>
+
+                    <a href='https://www.instagram.com/jack_atkerson/' target='_blank' rel='noreferrer'>
+                        <img className='instagram ftr-link' src={require('../../assets/images/instagram.png')} alt='Instagram'></img>
+                    </a>
+
+                    <br />
+                    
+                    <span>🔥Website by Jack, for Jack🔥</span>
                 </p>
             </div>
         </footer>
